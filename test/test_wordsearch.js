1fd4598c3bf7b59cf8f2d709ecd78e786579d4ba
Catherine Dawe <dawe.cm@gmail.com>
2021-02-10 17:23:04 -0500

    pair with @andyku25

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present horizontally", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,49 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return true if the word is present vertically", function () {
+    const result = wordSearch([
+      ['A', 'C', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'R', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'O', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'C', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'O', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'D', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'I', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'L', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'E', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CROCODILE')
+
+    assert.isTrue(result);
+  });
+
+
+  it("should return false if the word is not present vertically", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CROCODILE')
+
+    assert.isFalse(result);
+  });
+  it("should return undefined if the letter array is not provided", function() {
+    const result = wordSearch([], 'CROCODILE')
+
+    assert.isUndefined(result);
+  });
+
+
+
+
+
+
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  //combine arrays horizontally
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  if (letters.length === 0) return undefined;
+  // iterating through  each horizontal word
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  //callback tranpose for letters array
+  const columns = transpose(letters);
+  // joining
+  const verticalJoin = columns.map(ls => ls.join(''));
+  //console.log(verticalJoin)
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  return false;
+};
+
+const transpose = function(matrix) {
+  // Put your solution here
+  // Initialize an empty array
+  let outputArray = [];
+  // Loop through the rows of the matrix
+  for (let i = 0; i < matrix[0].length; i++) {
+    let colArray = [];
+    // Loop through the columns of the matrix
+    for (let j = 0; j < matrix.length; j++) {
+      colArray.push(matrix[j][i]);
+    }
+    outputArray.push(colArray);
+    //push colArray to outputArray
+  }
+  return outputArray;
+};
+
+module.exports = wordSearch;
+
+
+
